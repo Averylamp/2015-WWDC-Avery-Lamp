e1a594ca38af7fdc252555d1f4cc1a6c28d9d370
Avery Lamp <averylamp@gmail.com>
2015-04-18 22:21:20 -0400

    Initial Commit

---
+++ Avery LampTests/Avery_LampTests.m
@@ -0,0 +1,40 @@
+//
+//  Avery_LampTests.m
+//  Avery LampTests
+//
+//  Created by Avery Lamp on 4/18/15.
+//  Copyright (c) 2015 Avery Lamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Avery_LampTests : XCTestCase
+
+@end
+
+@implementation Avery_LampTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
